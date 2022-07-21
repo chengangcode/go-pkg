e01a1890f9b821a0b22be118165b09e10a1574f0
cheng <chengangxz111@163.com>
2022-07-21 15:39:09 +0800

    测试包11

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chengangcode/go-pkg
 
 go 1.17
+
+require github.com/go-basic/uuid v1.0.0
--- udpate.go
+++ udpate.go
@@ -1,7 +1,10 @@
 package go_pkg_test
 
 import "fmt"
+import "github.com/go-basic/uuid"
 
 func Update() {
 	fmt.Println(2131)
+	uuidStr := uuid.New()
+	fmt.Println(uuidStr)
 }
